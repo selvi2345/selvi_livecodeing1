13846901d738fc0789fc6be96dbc2f6aa4bc21f8
selvi2345 <vu.241fa04123@gmail.com>
2026-09-24 14:39:18 +0530

    Add functions for creating art and showing messages

---
+++ script.js
@@ -0,0 +1,7 @@
+function createArt() {
+    alert("🎨 Welcome to ArtNest! Your creative journey starts now.");
+}
+
+function showMessage() {
+    alert("👩🏻‍🎨 Artist Profile\n\nCreative Mind\n\nKeep creating and exploring!");
+}
